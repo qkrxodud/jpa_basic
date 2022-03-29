f89c921ff99f9d06919ed2a805a162c6c9fc6704
qkrxodud00 <qkrxodud00@gmail.com>
2022-03-29 19:38:58 +0900

    *구현 클래스마다 테이블 전략

--- src/main/java/helloJpa/Item.java
+++ src/main/java/helloJpa/Item.java
@@ -4,9 +4,10 @@ import javax.persistence.*;
 
 @Entity
 //@Inheritance(strategy = InheritanceType.JOINED)
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @DiscriminatorColumn
-public class Item {
+public abstract class Item {
 
     @Id @GeneratedValue
     private Long id;
